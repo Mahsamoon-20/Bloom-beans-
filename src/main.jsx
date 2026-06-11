6847b4cc2afda660082846804dbc4511237e15b3
Mahsamoon-20 <Mahsa.1996m@yahoo.com>
2026-06-11 07:47:03 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FloorPlan from './FloorPlan'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FloorPlan />
-  </React.StrictMode>
-)
