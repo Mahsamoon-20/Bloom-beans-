90a6df0c901c8f149153bf930fa8ba11ab928348
Mahsamoon-20 <Mahsa.1996m@yahoo.com>
2026-06-11 07:44:20 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FloorPlan from './FloorPlan'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FloorPlan />
+  </React.StrictMode>
+)
